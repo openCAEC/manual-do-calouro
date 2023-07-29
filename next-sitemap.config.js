2ee2ceba9a8be6e8a2df28237d452ee00c5a5314
jadsongmatos <jadson.g-matos@outlook.com>
2023-07-29 16:45:48 -0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://opencaec.github.io/manual-do-calouro/",
+  generateRobotsTxt: true,
+  changefreq: "monthly",
+};

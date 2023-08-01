046273f0ee0c2597bf5c1d39ef38888d532c4627
jadsongmatos <jadson.g-matos@outlook.com>
2023-08-01 15:53:40 -0300

    build

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,19 @@
- import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt">
-        <Head />
+        <Head>
+          <script src="planktos/install.js"></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
